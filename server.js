094e74ee591bfe0056b5a19e64be9b40eddc395b
salma <fatfieur18@gmail.com>
2021-07-16 00:53:20 +0100

    initial2

--- server.js
+++ server.js
@@ -36,6 +36,10 @@ app.use('/files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'));
 
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}.`);
+// app.listen(PORT, () => {
+//   console.log(`Listening on port ${PORT}.`);
+// });
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
